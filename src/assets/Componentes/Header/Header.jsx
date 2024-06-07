5e1410fd4489d71700904763f9c908107ff8c925
Leonardo-Marcilino <leonardo.marcilino@hotmail.com>
2024-06-07 05:00:30 -0300

    Correção de merge

--- src/assets/Componentes/Header/Header.jsx
+++ src/assets/Componentes/Header/Header.jsx
@@ -3,65 +3,73 @@ import "./Header.css";
 import Logo from "../../Images/Icons/Logo.png";
 
 function Header() {
-    return (
-        <header>
-            <nav className='MenuLinks'>
-                <div className='BoxLogo'>
-                    <a href=""><img src={Logo} alt="Logo da Intermentes" /></a>
-                    <p>Psicologia para todos</p>
+  return (
+    <header>
+      <nav className='MenuLinks'>
+        <Link to={''}>
+          <div className='BoxLogo'>
+            <a href=""><img src={Logo} alt="Logo da Intermentes" /></a>
+            <p>Psicologia para todos</p>
+          </div>
+        </Link>
+        <ul className='ListaLinks'>
+          <Link to={''}>
+            <li className='LinkItem'>
+              <a href="">Inicio</a>
+            </li>
+          </Link>
+          <li className='LinkItem LiDropMenu'>
+            <a>Serviços</a>
+            <div className='DropMenu'>
+              <a>
+                <div className='DropMenuBox'>
+                  <p className='TitleDropMenu Show'>Para especialistas</p>
+                  <p className='DescriptionDropMenu Show'>Compartilhe seu trabalho conosco e não saia perdendo</p>
                 </div>
-                <ul className='ListaLinks'>
-                    <li className='LinkItem'>
-                        <a href="">Inicio</a>
-                    </li>
-                    <li className='LinkItem LiDropMenu'>
-                        <a>Serviços</a>
-                        <div className='DropMenu'>
-                            <a>
-                                <div className='DropMenuBox'>
-                                    <p className='TitleDropMenu Show'>Para especialistas</p>
-                                    <p className='DescriptionDropMenu Show'>Compartilhe seu trabalho conosco e não saia perdendo</p>
-                                </div>
-                            </a>
-                            <Link to={'/Beneficiario'}>
-                                <div className='DropMenuBox'>
-                                    <p className='TitleDropMenu Show'>Para beneficiários</p>
-                                    <p className='DescriptionDropMenu Show'>O cuidado que precisa em apenas um lugar</p>
-                                </div>
-                            </Link>
-
-                            <Link to={'/PsicologiaGratuita'}>
-                                <div className='DropMenuBox'>
-                                    <p className='TitleDropMenu Show'>Psicologia Gratuita</p>
-                                    <p className='DescriptionDropMenu Show'>Aqui abraçamos aqueles que precisam e não tem condições</p>
-                                </div>
-                            </Link>
-                            <a href="">
-                                <div className='DropMenuBox'>
-                                    <p className='TitleDropMenu CorRoxa Show'>Agende sua consulta</p>
-                                    <p className='DescriptionDropMenu CorRoxa Show'>Encontre seu psicologo aqui e inicie a sua jornada</p>
-                                </div>
-                            </a>
-                        </div>
-                    </li>
-                    <li className='LinkItem '>
-                        <a href="">Parcerias</a>
-
-                    </li>
-                    <li className='LinkItem'>
-                        <a href="">Blog</a>
-                    </li>
-                    <li className='LinkItem'>
-                        <a href="">Sobre Nós</a>
-                    </li>
-                </ul>
-                <div className='BoxAcessar'>
-                <Link to={'/Login'}><a href="" className='Entrar'>Entrar</a></Link>
-                <Link to={'/Login'}><a href="" className='Cadastrar' >Cadastrar</a></Link>
+              </a>
+              <Link to={'/Beneficiario'}>
+                <div className='DropMenuBox'>
+                  <p className='TitleDropMenu Show'>Para beneficiários</p>
+                  <p className='DescriptionDropMenu Show'>O cuidado que precisa em apenas um lugar</p>
                 </div>
-            </nav>
-        </header>
-    )
+              </Link>
+
+              <Link to={'/PsicologiaGratuita'}>
+                <div className='DropMenuBox'>
+                  <p className='TitleDropMenu Show'>Psicologia Gratuita</p>
+                  <p className='DescriptionDropMenu Show'>Aqui abraçamos aqueles que precisam e não tem condições</p>
+                </div>
+              </Link>
+              <Link to={'/EncontrePsico'}>
+                <div className='DropMenuBox'>
+                  <p className='TitleDropMenu CorRoxa Show'>Agende sua consulta</p>
+                  <p className='DescriptionDropMenu CorRoxa Show'>Encontre seu psicologo aqui e inicie a sua jornada</p>
+                </div>
+              </Link>
+            </div>
+          </li>
+          <li className='LinkItem '>
+            <a href="">Parcerias</a>
+
+          </li>
+          <Link to={'/Blog'}>
+            <li className='LinkItem'>
+              <a href="">Blog</a>
+            </li>
+          </Link>
+          <Link to={'/SobreNos'}>
+            <li className='LinkItem'>
+              <a href="">Sobre Nós</a>
+            </li>
+          </Link>
+        </ul>
+        <div className='BoxAcessar'>
+          <Link to={'/Login'}><a href="" className='Entrar'>Entrar</a></Link>
+          <Link to={'/Login'}><a href="" className='Cadastrar' >Cadastrar</a></Link>
+        </div>
+      </nav>
+    </header>
+  )
 }
 
 export default Header;
