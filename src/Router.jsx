c4297731b91d255f3f0ab5156b9265893583da18
Reginaldo Ferreira Junior <ferreirajuniorreginaldo@gmail.com>
2024-06-06 23:41:00 -0300

    PreviaPerfilEsobre

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,28 +1,34 @@
-import { Routes, Route } from 'react-router-dom'
-import Beneficiario from './assets/Pages/Beneficiario/Beneficiario'
-import PsicologiaGratuita from './assets/Pages/PsicologiaGratuita/PsicologiaGratuita'
-import Login from './assets/Pages/Login/Login'
-import Especialista from './assets/Pages/Especialista/Especialista'
+import { Routes, Route } from "react-router-dom";
+import Beneficiario from "./assets/Pages/Beneficiario/Beneficiario";
+import PsicologiaGratuita from "./assets/Pages/PsicologiaGratuita/PsicologiaGratuita";
+import Login from "./assets/Pages/Login/Login";
+import Especialista from "./assets/Pages/Especialista/Especialista";
+import SobreNos from "./assets/Pages/SobreNos/SobreNos.jsx";
+import Home from "./assets/Pages/Home/Home.jsx";
+import EncontrePsico from "./assets/Pages/AgendeConsulta/EncontrePsico.jsx";
+import Perfil from "./assets/Pages/UsuarioLogado/Perfil";
+import PainelControle from "./assets/Componentes/OpçõesPerfil/PainelControle";
 
-import Perfil from './assets/Pages/UsuarioLogado/Perfil'
-import PainelControle from './assets/Componentes/OpçõesPerfil/PainelControle'
+export default function MainRoutes() {
+  return (
+    <Routes>
+      <Route path="/Home" element={<Home />}></Route>
 
-export default function MainRoutes(){
-    return(
-        <Routes>
-            <Route path='/Especialista' element={<Especialista/>}></Route>
-            <Route path='/Beneficiario' element={<Beneficiario />}></Route>
-            <Route path='/PsicologiaGratuita' element={<PsicologiaGratuita />}></Route>
-            <Route path='/Login' element={<Login />}></Route>
+      <Route path="/Especialista" element={<Especialista />}></Route>
+      <Route path="/Beneficiario" element={<Beneficiario />}></Route>
+      <Route
+        path="/PsicologiaGratuita"
+        element={<PsicologiaGratuita />}></Route>
+      <Route path="/Login" element={<Login />}></Route>
 
-            {/* Roda dentro do perfil do usuário */}
-
-            <Route path='/Perfil' element={<Perfil />}>
-                <Route path='/Perfil/PainelDeControle' element={<PainelControle />}></Route>
-            </Route>
-            
-
-        </Routes>
-    )
-
+      {/* Roda dentro do perfil do usuário */}
+      <Route path="/EncontrePsico" element={<EncontrePsico />}></Route>
+      <Route path="/SobreNos" element={<SobreNos />}></Route>
+      <Route path="/Perfil" element={<Perfil />}>
+        <Route
+          path="/Perfil/PainelDeControle"
+          element={<PainelControle />}></Route>
+      </Route>
+    </Routes>
+  );
 }
---
+++ src/assets/Pages/AgendeConsulta/EncontrePsico.jsx
@@ -0,0 +1,11 @@
+import CorpoPagina from "./CorpoPagina";
+
+function EncontrePsico() {
+  return (
+    <main>
+      <CorpoPagina />
+    </main>
+  );
+}
+
+export default EncontrePsico;
