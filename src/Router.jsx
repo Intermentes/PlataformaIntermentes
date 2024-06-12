33cc6c0d9acd9e5acfb982cded79e2638252d85a
Leonardo-Marcilino <leonardo.marcilino@hotmail.com>
2024-06-12 19:59:43 -0300

    correção deploy

--- src/Router.jsx
+++ src/Router.jsx
@@ -5,11 +5,11 @@ import PsicologiaGratuita from "./assets/Pages/PsicologiaGratuita/PsicologiaGrat
 import Login from "./assets/Pages/Login/Login";
 import Especialista from "./assets/Pages/Especialista/Especialista";
 import Home from "./assets/Pages/Home/Home";
-import Blog from "./assets/Pages/Blog/Blog";
-import Blog_P1 from "./assets/Pages/Blog/Blog_p1.jsx";
-import Blog_P2 from "./assets/Pages/Blog/Blog_p2.jsx";
-import Blog_P3 from "./assets/Pages/Blog/Blog_p3.jsx";
-import Blog_P4 from "./assets/Pages/Blog/Blog_p4.jsx";
+// import Blog from "./assets/Pages/Blog/Blog";
+// import Blog_P1 from "./assets/Pages/Blog/Blog_p1.jsx";
+// import Blog_P2 from "./assets/Pages/Blog/Blog_p2.jsx";
+// import Blog_P3 from "./assets/Pages/Blog/Blog_p3.jsx";
+// import Blog_P4 from "./assets/Pages/Blog/Blog_p4.jsx";
 import SobreNos from "./assets/Pages/SobreNos/SobreNos.jsx";
 import EncontrePsico from "./assets/Pages/AgendeConsulta/EncontrePsico.jsx";
 import Perfil from "./assets/Pages/UsuarioLogado/Perfil";
@@ -25,11 +25,11 @@ export default function MainRoutes() {
       <Route path="/Especialista" element={<Especialista />} />
       <Route path='/PsicologiaGratuita' element={<PsicologiaGratuita />}></Route>
       <Route path='/Login' element={<Login />}></Route>
-      <Route path="/Blog" element={<Blog />} />
+      {/* <Route path="/Blog" element={<Blog />} />
       <Route path="/Blog_P1" element={<Blog_P1 />} />
       <Route path="/Blog_P2" element={<Blog_P2 />} />
       <Route path="/Blog_P3" element={<Blog_P3 />} />
-      <Route path="/Blog_P4" element={<Blog_P4 />} />
+      <Route path="/Blog_P4" element={<Blog_P4 />} /> */}
       <Route path="/EncontrePsico" element={<EncontrePsico />} />
       <Route path="/SobreNos" element={<SobreNos />} />
 
