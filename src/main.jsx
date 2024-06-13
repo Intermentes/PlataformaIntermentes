1314431b470e0a2ce8f634c270cd45219bdc7387
bia-pequeno <bia.2017pequeno@gmail.com>
2024-06-12 21:07:20 -0300

    tentativa3

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import './reset.css'
 import './root.css'
 import Header from './assets/Componentes/Header/Header'
 import Footer from './assets/Componentes/Footer/Footer'
-import Especialistas from './assets/Pages/Especialista/EspecialistaLogado'
 
 
 
@@ -17,7 +16,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Router>
         <Header/>
         <Routes />
-        <Especialistas/>
         <Footer />
     </Router>
   </React.StrictMode>
