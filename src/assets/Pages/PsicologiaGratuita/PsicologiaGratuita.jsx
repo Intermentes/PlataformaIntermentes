a9751c799b855e88316599d1e2451a68103f0905
Leonardo-Marcilino <leonardo.marcilino@hotmail.com>
2024-06-20 01:17:08 -0300

    correção de font e a página de psicologia gratuita

--- src/assets/Pages/PsicologiaGratuita/PsicologiaGratuita.jsx
+++ src/assets/Pages/PsicologiaGratuita/PsicologiaGratuita.jsx
@@ -46,116 +46,94 @@ export default function PsicologiaGratuita() {
                             <a href="https://www.casaum.org/" target='_blank' className={styles.BtnOng}>Saiba Mais</a>
                         </div>
                     </div>
-                    {/* ... outros blocos de BoxOng seguem o mesmo padrão */}
+                    <div className={styles.BoxOng}>
+                        <div className={styles.BoxLogoOng}>
+                            <div className={styles.BoxImg}>
+                                <img src={OngIpefem} alt="" />
+                            </div>
+                        </div>
+                        <div className={styles.BoxDescriptionOng}>
+                            <div className={styles.GroupNameLocalization}>
+                                <h3>Ipefem</h3>
+                            </div>
+                            <p>O Ipefem é uma Organização Não Governamental (ONG) de educação em saúde mental criada para ajudar pessoas a identificar e interromper os processos de violência socioemocional no trabalho – principalmente as microagressões contra mulheres.</p>
+                            <a href="https://ipefem.org.br/" target='_blank' className={styles.BtnOng}>Saiba Mais</a>
+                        </div>
+                    </div>
+                    <div className={styles.BoxOng}>
+                        <div className={styles.BoxLogoOng}>
+                            <div className={styles.BoxImg}>
+                                <img src={OngCvv} alt="" />
+                            </div>
+                        </div>
+                        <div className={styles.BoxDescriptionOng}>
+                            <div className={styles.GroupNameLocalization}>
+                                <h3>CVV (Centro de Valorização da Vida)</h3>
+                            </div>
+                            <p>O CVV – Centro de Valorização da Vida realiza apoio emocional e prevenção do suicídio, atendendo voluntária e gratuitamente todas as pessoas que querem e precisam conversar, sob total sigilo por telefone, e-mail e chat 24 horas todos os dias. Ligue 188.</p>
+                            <a href="https://cvv.org.br/conheca-mais/" target='_blank' className={styles.BtnOng}>Saiba Mais</a>
+                        </div>
+                    </div>
+                    <div className={styles.BoxOng}>
+                        <div className={styles.BoxLogoOng}>
+                            <div className={styles.BoxImg}>
+                                <img src={OngCasasMarias} alt="" />
+                            </div>
+                        </div>
+                        <div className={styles.BoxDescriptionOng}>
+                            <div className={styles.GroupNameLocalization}>
+                                <h3>Casa de Marias</h3>
+                            </div>
+                            <p>A Casa de Marias é um projeto sem fins lucrativos que surgiu na Vila Esperança, zona leste da cidade de São Paulo. Com a equipe formada por um grupo de psicoterapeutas, o foco do projeto é no atendimento à população negra, periférica, que reside na região, especialmente mulheres.</p>
+                            <a href="https://www.instagram.com/casademariasoficial/" target='_blank' className={styles.BtnOng}>Saiba Mais</a>
+                        </div>
+                    </div>
+                    <div className={styles.BoxOng}>
+                        <div className={styles.BoxLogoOng}>
+                            <div className={styles.BoxImg}>
+                                <img src={OngBorboletaAzul} alt="" />
+                            </div>
+                        </div>
+                        <div className={styles.BoxDescriptionOng}>
+                            <div className={styles.GroupNameLocalization}>
+                                <h3>Instituto Borboleta Azul</h3>
+                            </div>
+                            <p>O objetivo do Instituto Borboleta Azul é oferecer atendimento psicológico gratuito e qualificado, dando acesso à saúde mental à jovens e suas famílias em situação de vulnerabilidade social.</p>
+                            <a href="https://institutoborboletaazul.org.br/" target='_blank' className={styles.BtnOng}>Saiba Mais</a>
+                        </div>
+                    </div>
+                    <div className={styles.BoxOng}>
+                        <div className={styles.BoxLogoOng}>
+                            <div className={styles.BoxImg}>
+                                <img src={OngBemEstar} alt="" />
+                            </div>
+                        </div>
+                        <div className={styles.BoxDescriptionOng}>
+                            <div className={styles.GroupNameLocalization}>
+                                <h3>Instituto Bem do Estar</h3>
+                            </div>
+                            <p>O Instituto Bem do Estar tem como propósito desafiar as pessoas a mudar o seu comportamento em relação à saúde da mente, colaborando para a prevenção de doenças psicológicas e contribuindo para uma sociedade mais consciente e saudável.</p>
+                            <a href="https://www.bemdoestar.org/" target='_blank' className={styles.BtnOng}>Saiba Mais</a>
+                        </div>
+                    </div>
+                    <div className={styles.BoxOng}>
+                        <div className={styles.BoxLogoOng}>
+                            <div className={styles.BoxImg}>
+                                <img src={OngAusOuvidos} alt="" />
+                            </div>
+                        </div>
+                        <div className={styles.BoxDescriptionOng}>
+                            <div className={styles.GroupNameLocalization}>
+                                <h3>Instituto Aus ouvidos</h3>
+                            </div>
+                            <p>Aus Ouvidos, é uma plataforma digital que oferece um serviço especializado de escutas pontuais que incide no cuidado emocional, físico e social das pessoas.</p>
+                            <a href="https://www.ausouvidos.com/" target='_blank' className={styles.BtnOng}>Saiba Mais</a>
+                        </div>
+                    </div>
+    
                 </div>
             </section>
             <Footer />
         </main>
     );
 }
-
-
-            {/* <section className='SectionOngs'>
-                <h2>ONG'S</h2>
-                <div className='ContainerOngs'>
-                    <div className='BoxOng'>
-                        <div className='BoxLogoOng'>
-                            <div className='BoxImg'>
-                                <img src={OngCasa1} alt="" /> 
-                            </div>
-                        </div>
-                        <div className='BoxDescriptionOng'>
-                            <div className='GroupNameLocalization'>
-                                <h3>Casa 1</h3>
-                            </div>
-                            <p>Entre os serviços ofertados pela Clínica Social Casa 1 está atendimento psicoterápico continuado, atendimento psiquiátrico, acompanhamento com nutricionistas, plantão de escuta e diversas modalidades de terapias complementares.</p>
-                            <a href="https://www.casaum.org/" target='_blank' className='BtnOng'>Saiba Mais</a>
-                        </div>
-                    </div>
-                    <div className='BoxOng'>
-                        <div className='BoxLogoOng'>
-                            <div className='BoxImg'>
-                                <img src={OngIpefem} alt="" /> 
-                            </div>
-                        </div>
-                        <div className='BoxDescriptionOng'>
-                            <div className='GroupNameLocalization'>
-                                <h3>Ipefem</h3>
-                            </div>
-                            <p>O Ipefem é uma Organização Não Governamental (ONG) de educação em saúde mental criada para ajudar pessoas a identificar e interromper os processos de violência socioemocional no trabalho – principalmente as microagressões contra mulheres.</p>
-                            <a href="https://ipefem.org.br/" target='_blank' className='BtnOng'>Saiba Mais</a>
-                        </div>
-                    </div>
-                    <div className='BoxOng'>
-                        <div className='BoxLogoOng'>
-                            <div className='BoxImg'>
-                                <img src={OngCvv} alt="" /> 
-                            </div>
-                        </div>
-                        <div className='BoxDescriptionOng'>
-                            <div className='GroupNameLocalization'>
-                                <h3>CVV (Centro de Valorização da Vida)</h3>
-                            </div>
-                            <p>O CVV – Centro de Valorização da Vida realiza apoio emocional e prevenção do suicídio, atendendo voluntária e gratuitamente todas as pessoas que querem e precisam conversar, sob total sigilo por telefone, e-mail e chat 24 horas todos os dias. Ligue 188.</p>
-                            <a href="https://cvv.org.br/conheca-mais/" target='_blank' className='BtnOng'>Saiba Mais</a>
-                        </div>
-                    </div>
-                    <div className='BoxOng'>
-                        <div className='BoxLogoOng'>
-                            <div className='BoxImg'>
-                                <img src={OngCasasMarias} alt="" /> 
-                            </div>
-                        </div>
-                        <div className='BoxDescriptionOng'>
-                            <div className='GroupNameLocalization'>
-                                <h3>Casa de Marias</h3>
-                            </div>
-                            <p>A Casa de Marias é um projeto sem fins lucrativos que surgiu na Vila Esperança, zona leste da cidade de São Paulo. Com a equipe formada por um grupo de psicoterapeutas, o foco do projeto é no atendimento à população negra, periférica, que reside na região, especialmente mulheres.</p>
-                            <a href="https://www.instagram.com/casademariasoficial/" target='_blank' className='BtnOng'>Saiba Mais</a>
-                        </div>
-                    </div>
-                    <div className='BoxOng'>
-                        <div className='BoxLogoOng'>
-                            <div className='BoxImg'>
-                                <img src={OngBorboletaAzul} alt="" /> 
-                            </div>
-                        </div>
-                        <div className='BoxDescriptionOng'>
-                            <div className='GroupNameLocalization'>
-                                <h3>Instituto Borboleta Azul</h3>
-                            </div>
-                            <p>O objetivo do Instituto Borboleta Azul é oferecer atendimento psicológico gratuito e qualificado, dando acesso à saúde mental à jovens e suas famílias em situação de vulnerabilidade social.</p>
-                            <a href="https://institutoborboletaazul.org.br/" target='_blank' className='BtnOng'>Saiba Mais</a>
-                        </div>
-                    </div>
-                    <div className='BoxOng'>
-                        <div className='BoxLogoOng'>
-                            <div className='BoxImg'>
-                                <img src={OngBemEstar} alt="" /> 
-                            </div>
-                        </div>
-                        <div className='BoxDescriptionOng'>
-                            <div className='GroupNameLocalization'>
-                                <h3>Instituto Bem do Estar</h3>
-                            </div>
-                            <p>O Instituto Bem do Estar tem como propósito desafiar as pessoas a mudar o seu comportamento em relação à saúde da mente, colaborando para a prevenção de doenças psicológicas e contribuindo para uma sociedade mais consciente e saudável.</p>
-                            <a href="https://www.bemdoestar.org/" target='_blank' className='BtnOng'>Saiba Mais</a>
-                        </div>
-                    </div>
-                    <div className='BoxOng'>
-                        <div className='BoxLogoOng'>
-                            <div className='BoxImg'>
-                                <img src={OngAusOuvidos} alt="" /> 
-                            </div>
-                        </div>
-                        <div className='BoxDescriptionOng'>
-                            <div className='GroupNameLocalization'>
-                                <h3>Instituto Aus ouvidos</h3>
-                            </div>
-                            <p>Aus Ouvidos, é uma plataforma digital que oferece um serviço especializado de escutas pontuais que incide no cuidado emocional, físico e social das pessoas.</p>
-                            <a href="https://www.ausouvidos.com/" target='_blank' className='BtnOng'>Saiba Mais</a>
-                        </div>
-                    </div>
-                </div>
-            </section> */}
-
