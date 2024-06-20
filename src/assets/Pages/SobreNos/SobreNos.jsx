40c5d8489c6ac2d38d4a040491885a98c67fee91
bia-pequeno <bia.2017pequeno@gmail.com>
2024-06-20 00:04:25 -0300

    sobre att

--- src/assets/Pages/SobreNos/SobreNos.jsx
+++ src/assets/Pages/SobreNos/SobreNos.jsx
@@ -3,6 +3,9 @@ import Devs from "./Desenvolvedores.jsx";
 import Acesso from './Acesso_Psicologia.jsx';
 import Header from "../../Componentes/Header/Header"
 import Footer from "../../Componentes/Footer/Footer"
+import ODS from '../../Images/SobreNosImg/ODS.png'
+import PlanoFuturo from '../../Images/SobreNosImg/PlanoFuturo.png'
+import style from './SobreNos.module.css'
 
 function SobreNos() {
   return (
@@ -11,6 +14,11 @@ function SobreNos() {
       <Acesso />
       <Valores />
       <Devs />
+      <div className={style.slide}>
+        <img src={ODS} alt=""/>
+        <img src={PlanoFuturo} alt="" />        
+      </div>
+
       <Footer/>
     </main>
   );
